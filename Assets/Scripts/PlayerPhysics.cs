1874a8124622e0e9e85c5ebaa414738015c3f991
swcgogo0308 <swcgogo0308@naver.com>
2018-01-23 04:09:30 +0900

    Genji_1
    1. 이동 (GetAxis, Rigid)
    2. 점프 (GetKeyDown, Rigid.AddForce)
    2-1. 2단 점프 (코루틴[점프 딜레이], 충돌 검사 메소드[raycast, bool])

---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPhysics : MonoBehaviour {
+	public float speed = 10f; 
+
+	[Header("JumpSettings")]
+	public int maxJumpCount = 2;
+	public float jumpForce = 5f;
+	public float jumpDelayTime = 1f;
+
+	[Space(10)]
+	public float onGroundRange = 2f;
+
+	Rigidbody rigid;
+
+	Vector3 movement;
+
+	bool isOnGround;
+	bool isjumpDelaying;
+
+	int currentJumpCount;
+
+	// Use this for initialization
+	void Awake () {
+		rigid = GetComponent<Rigidbody> ();
+		currentJumpCount = maxJumpCount;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		MovementPhysics ();
+		JumpPhysics ();
+	}
+
+	void Update()
+	{
+		CheckOnGround ();
+	}
+
+	void MovementPhysics() {
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float vertical = Input.GetAxisRaw ("Vertical");
+		
+		movement = new Vector3 (horizontal, 0, vertical);
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		rigid.MovePosition (transform.position + movement);
+	}
+
+	void CheckOnGround() {
+		if (Physics.Raycast (transform.position, Vector3.down, onGroundRange, 1 << 8)) {
+			isOnGround = true;
+			currentJumpCount = maxJumpCount;
+		} else
+			isOnGround = false;
+	}
+
+	#region JumpMathod
+
+	void JumpPhysics() {
+		if (!Input.GetKeyDown (KeyCode.Space) || isjumpDelaying || (!isOnGround && currentJumpCount <= 0))
+			return;
+		
+		currentJumpCount--;
+		rigid.AddForce (Vector3.up * jumpForce, ForceMode.Impulse);
+		StartCoroutine ("JumpDelay");
+
+	}
+
+	IEnumerator JumpDelay() {
+		isjumpDelaying = true;
+		yield return new WaitForSeconds (jumpDelayTime);
+		isjumpDelaying = false;
+	}
+
+	#endregion
+		
+	void SkillPhysics() {
+		//TODO Skills
+	}
+
+}
